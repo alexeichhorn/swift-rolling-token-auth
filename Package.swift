bac06358c35d5aa6c2d3b84bf296ad35dd84db60
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-02-23 12:16:08 +0100

    GitHub CI

--- Package.swift
+++ Package.swift
@@ -5,9 +5,11 @@ let package = Package(
     name: "RollingTokenAuth",
     platforms: [
         .iOS(.v13),
+        .macCatalyst(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6),
+        .visionOS(.v1),
     ],
     products: [
         .library(
